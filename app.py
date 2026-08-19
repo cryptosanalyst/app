479a3768f8467888f44b390a70bf534f355f28e3
cryptosanalyst <cryptosanalyst@gmail.com>
2026-08-19 08:58:34 +0000

    Update app.py

--- app.py
+++ app.py
@@ -139,7 +139,7 @@ if avatar_filename:
     """, unsafe_allow_html=True)
 
 st.markdown("<h1>Cryptos Analyst IA</h1>", unsafe_allow_html=True)
-st.markdown("<div class='welcome-msg'>Bienvenue je suis l'agent IA de cryptos analyst je vous aide à analyser rapidement vos projets crypto</div>", unsafe_allow_html=True)
+st.markdown("<div class='welcome-msg'>Bienvenu(e) je suis l'agent IA de cryptos analyst je vous aide à analyser rapidement vos projets crypto</div>", unsafe_allow_html=True)
 
 # ---------------------------------------------------------
 # OPTION A : Limite de 2 requêtes / jour par session
